882de94e47fb0741f21d558c4f75ced1d3a00554
John Biddiscombe <biddisco@cscs.ch>
2020-10-14 22:38:36 +0200

    Add missing #include <stdio>

--- src/unitiger/hydro_impl/hydro_cuda/reconstruct_cuda_kernel.cu
+++ src/unitiger/hydro_impl/hydro_cuda/reconstruct_cuda_kernel.cu
@@ -1,5 +1,7 @@
 #ifdef OCTOTIGER_HAVE_CUDA
 
+#include <stdio.h>
+
 __global__ void kernel_reconstruct(double *Q, double *D1, double *U_, double *X, double omega) {
     bool first_thread = (blockIdx.x == 0) && (threadIdx.y == 0) && (threadIdx.z == 0);
     if (first_thread)
